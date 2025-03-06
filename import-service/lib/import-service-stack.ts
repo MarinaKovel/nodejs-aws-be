e1ae167bf9a7db3f93502ffe365e2d0346db8e96
Marina Kovel <m.kovel@softteco.com>
2025-03-06 10:08:18 +0300

    feat: add import service and importProductsFile lambda

--- import-service/lib/import-service-stack.ts
+++ import-service/lib/import-service-stack.ts
@@ -1,16 +1,60 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as path from 'path';
 
 export class ImportServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create or reference your S3 bucket
+    const bucket = s3.Bucket.fromBucketName(this, 'ImportBucket', 'aws-be-import-service-bucket');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ImportServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create the importProductsFile Lambda function
+    const importProductsFileLambda = new NodejsFunction(this, 'ImportProductsFileFunction', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'importProductsFile',
+      entry: path.join(__dirname, '../src/functions/importProductsFile/handler.ts'),
+      environment: {
+        BUCKET_NAME: bucket.bucketName,
+        REGION: this.region,
+      },
+      bundling: {
+        externalModules: [],
+        minify: true,
+        sourceMap: true,
+      },
+    });
+
+    // Grant S3 permissions to the Lambda function
+    bucket.grantReadWrite(importProductsFileLambda);
+
+    // Create API Gateway
+    const api = new apigateway.RestApi(this, 'ImportApi', {
+      restApiName: 'Import Service',
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+      },
+    });
+
+    // Create the /import resource
+    const importResource = api.root.addResource('import');
+
+    // Add the GET method to the /import resource
+    importResource.addMethod('GET', new apigateway.LambdaIntegration(importProductsFileLambda), {
+      requestParameters: {
+        'method.request.querystring.name': true,
+      },
+    });
+
+    // Output the API URL
+    new cdk.CfnOutput(this, 'ApiUrl', {
+      value: api.url,
+      description: 'The URL of the Import API',
+    });
   }
 }
