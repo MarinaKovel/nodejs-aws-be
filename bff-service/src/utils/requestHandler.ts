38bd82d738a58e5ff38c956b27f41a5a71a62c6e
Marina Kovel <m.kovel@softteco.com>
2025-04-17 17:42:16 +0300

    feat: add cache service

--- bff-service/src/utils/requestHandler.ts
+++ bff-service/src/utils/requestHandler.ts
@@ -1,5 +1,6 @@
 import { IncomingMessage, ServerResponse, OutgoingHttpHeaders } from 'http';
 import axios, { AxiosError, AxiosRequestConfig } from 'axios';
+import { cacheService } from './cacheService';
 
 interface RequestHandlerParams {
   req: IncomingMessage;
@@ -35,8 +36,30 @@ export const handleRequest = async ({
       const path = queryParams.get('path')?.slice(1);
       if (path) targetUrl += path;
     }
-
+    console.log('Request method:', req.method);
+    console.log('servicename', serviceName);
     console.log(`Forwarding request to: ${targetUrl}`);
+    console.log('Check headers:', req.headers)
+    console.log('Check auth header:', req.headers.authorization ?? 'No auth header')
+
+      // Check if it's a products list request
+    const isProductsList = serviceName === 'product' &&  req.method === 'GET';
+
+    console.log('isProductlist', isProductsList);
+    
+    // If it's a products list request, try to get from cache
+    if (isProductsList) {
+      const cachedData = cacheService.get<any>('productsList');
+      if (cachedData) {
+        console.log('Serving from cache');
+        res.writeHead(200, {
+        'Content-Type': 'application/json',
+        'X-Cache': 'HIT'
+        });
+        res.end(JSON.stringify(cachedData));
+        return;
+      }
+    }
 
     const axiosConfig: AxiosRequestConfig = {
       method: req.method || 'GET',
@@ -47,21 +70,34 @@ export const handleRequest = async ({
           'Authorization': req.headers.authorization
         })
       },
-      validateStatus: () => true, // Don't throw on non-2xx responses
+      validateStatus: () => true,
       maxRedirects: 5,
-      timeout: 5000 // 5 second timeout
+      timeout: 10000
     };
 
     // Only add body for POST, PUT, PATCH methods
     if (body && ['POST', 'PUT', 'PATCH'].includes(req.method || '')) {
       axiosConfig.data = body;
     }
-
+    console.log('Axios config:', axiosConfig);
     const response = await axios(axiosConfig);
+
+    // If it's a successful products list request, cache the response
+    if (isProductsList && response.status === 200) {
+      console.log('Caching products list');
+      cacheService.set('productsList', response.data);
+    }
+
+    // If it's a successful product creation, invalidate the cache
+    if (serviceName === 'product' && req.method === 'POST' && response.status === 200) {
+      console.log('Invalidating products list cache');
+      cacheService.del('productsList');
+    }
 
     // Convert Axios headers to OutgoingHttpHeaders
     const responseHeaders: OutgoingHttpHeaders = {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'X-Cache': isProductsList ? 'MISS' : 'BYPASS'
     };
 
     // Safely copy headers
